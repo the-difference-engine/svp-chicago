feb9e9f7b217429dd23a34ba0385f9182c0a1a01
Bobbie <obobiec@gmail.com>
2016-04-11 18:10:08 -0500

    add to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'landings/index'
+  get '/landings' => 'landings#index'
+
+  get '/ratings' => 'ratings#index'
+  get '/ratings/:id' => 'ratings#show'
+
+  get '/ratings/new' => 'ratings#new'
+  post '/ratings' => 'ratings#create'
+
+  get '/ratings/:id/edit' => 'ratings#edit'
+  patch '/ratings/:id' => ''
+
 
   get 'users/index'
 
