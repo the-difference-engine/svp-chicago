2c02a7b45c1b7609fbc9813fd318203e49e7617b
heydanhey <danhey@gmail.com>
2016-05-06 09:46:27 -0500

    json bug fix

--- app/controllers/api/v1/lois_controller.rb
+++ app/controllers/api/v1/lois_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::LoisController < ApplicationController
-	# skip_before_action :verify_authenticity_token, only: [:index]
+	skip_before_action :verify_authenticity_token, only: [:index]
   def index
   	@lois = Loi.all
   end
