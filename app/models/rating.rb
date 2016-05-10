f8d1c508b12dbbe12ce9ad89a23b9df080ad82dc
heydanhey <danhey@gmail.com>
2016-05-10 10:15:28 -0500

    Added CSV File download for Ratings and updated CSV for LOIs

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,4 +6,15 @@ class Rating < ActiveRecord::Base
 		total = q1 + q2 + q3 + q4 
 		total/4.to_f
 	end
+
+  def self.to_csv
+    attributes = %w{id q1 q2 q3 q4 q5 weighted_score}
+   
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.each do |rating|
+        csv << rating.attributes.values_at(*attributes)
+      end
+    end
+  end
 end
