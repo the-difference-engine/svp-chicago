ef6645f0f49aebbbffb87ce05e0e62ce785b9ada
santamariaa <asantamaria101@gmail.com>
2016-05-09 19:07:59 -0500

    added env varialbles to production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
     :authentication => :plain,
     :address => "smtp.mailgun.org",
     :port => 587,
-    :domain => ENV['MAILGUN_API_KEY'],
+    :domain => ENV['MAILGUN_DOMAIN'],
     :user_name => ENV['MAILGUN_USER_NAME'],
     :password => ENV['MAILGUN_PASSWORD']
   }
