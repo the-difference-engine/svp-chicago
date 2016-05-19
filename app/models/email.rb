440921e6fad177350d7192f072f42f3de69bd049
Chris Connell <chris.g.connell@gmail.com>
2016-05-19 18:07:45 -0500

    before merge

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,29 +1,14 @@
-# class Email
+class Email
 
-#   # def initialize(loi)
-#   #   @email = loi
-#   # end
+  # Mail.defaults do
+  #     delivery_method :smtp, {
+  #     :address => 'smtp.gmail.com',
+  #     :port => '587',
+  #     :user_name => ENV['GMAILUSER'],
+  #     :password => ENV['GMAILPASSWORD'],
+  #     :authentication => :plain,
+  #     :enable_starttls_auto => true
+  #     }
+  # end   
 
-#   def send
-#      Mail.defaults do
-#       delivery_method :smtp, {
-#       :address => 'smtp.gmail.com',
-#       :port => '587',
-#       :user_name => '',
-#       :password => '',
-#       :authentication => :plain,
-#       :enable_starttls_auto => true
-#       }
-#     end
-
-#       mail = Mail.new do
-#       from     'SVP'
-#       #need to insert email that was submitted with form
-#       to       @email
-#       subject  'Your SVP submission was received!'
-#       end
-
-#       mail.deliver!
-#   end    
-
-# end
+end
