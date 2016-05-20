d5328cca27bd64e6e722e1910679133493f5ece9
Chris Connell <chris.g.connell@gmail.com>
2016-05-19 19:39:55 -0500

    user landing showing user's lois

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160520001527) do
 
   # These are extensions that must be enabled in order to support this database
@@ -47,8 +46,8 @@ ActiveRecord::Schema.define(version: 20160520001527) do
     t.string   "email"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.boolean  "submitted",  default: false
     t.integer  "user_id"
+    t.boolean  "submitted",  default: false
   end
 
   create_table "questions", force: :cascade do |t|
