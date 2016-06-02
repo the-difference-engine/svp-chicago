6555a49dcf8e511c3cc11c8c24f1ae58146cad41
heydanhey <danhey@gmail.com>
2016-06-02 12:06:18 -0500

    added admin invites to user index

--- app/controllers/lois_controller.rb
+++ app/controllers/lois_controller.rb
@@ -14,50 +14,12 @@
   end
 
   def new
-    # @loi  = Loi.new
-    # @questions = Question.order(:id).all 
-    # @sections = Section.order(:id).all 
+    
   end
 
   def create
-    # @loi = Loi.new({name: params[:name], email: params[:email]})
 
-    # if answers_valid?
-    #   if @loi.save
-
-    #     params[:answers].each do |key, value|
-
-    #       answer = Answer.create({loi_id: @loi.id, question_id: key, answer: value})
-
-    #       if value.include?("challenges")
-    #         i = 0
-    #         (value["challenges"].length / 2).times do
-    #           Challenge.create({answer_id: answer.id, challenge: value["challenges"][i], priority: value["challenges"][i+(value["challenges"].length / 2)]})
-    #           i += 1
-    #         end
-    #       end
-
-    #       if value.include?("ftes")
-    #         Fte.create({answer_id: answer.id, amount_1: value["ftes"][0], amount_2: value["ftes"][1], amount_3: value["ftes"][2]})
-    #       end
-    #     end
-
-    #     # NotifierMailer.welcome_email(@loi).deliver_now
-    #     flash[:success] = "LOI Created"
-    #     redirect_to "/lois/#{@loi.id}"
-    #   else
-    #     flash[:error] = @loi.errors.full_messages
-    #     render :new
-    #   end
-    # else
-    #   #errors presented here
-    #   render :new
-    # end
   end
-
-  # def answers_valid?
-  #   params[:answers].select{|key, value| value.empty?}.empty?
-  # end
 
   def show
     # @sections = Section.order(:id).all
