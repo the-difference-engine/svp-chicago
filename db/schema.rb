250e8fcdb985d006ce54841a1204a87b5de2da94
Chris Connell <chris.g.connell@gmail.com>
2016-06-15 10:27:32 -0500

    added migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607000936) do
+ActiveRecord::Schema.define(version: 20160615013703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,8 @@ ActiveRecord::Schema.define(version: 20160607000936) do
     t.string   "email"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.boolean  "submitted",  default: false
     t.integer  "user_id"
+    t.boolean  "submitted",  default: false
   end
 
   create_table "questions", force: :cascade do |t|
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 20160607000936) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "super_admin",            default: false
+    t.datetime "deleted_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
