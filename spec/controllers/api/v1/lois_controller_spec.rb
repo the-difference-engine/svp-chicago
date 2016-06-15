d59fc9e37abf1c0977a79e9240219835e50c8d22
Chris Connell <chris.g.connell@gmail.com>
2016-06-14 19:14:26 -0500

    mail test functioning

--- spec/controllers/api/v1/lois_controller_spec.rb
+++ spec/controllers/api/v1/lois_controller_spec.rb
@@ -1,19 +1,62 @@
 require 'rails_helper'
 
+
+
 RSpec.describe Api::V1::LoisController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+def login_user
+  @request.env["devise.mapping"] = Devise.mappings[:user]
+  sign_in FactoryGirl.create(:user)
+end
+
+Mail.defaults do
+  delivery_method :test # in practice you'd do this in spec_helper.rb
+end
+
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET #show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "create loi" do
+  #   it "sends email" do
+  #     expect(Mail::TestMailer.deliveries).to be_empty
+  #   end
+  # end
+
+  describe "save loi" do
+
+    before :each do
+      login_user
+    end
+
+    it "sends email" do
+
+        answer = FactoryGirl.build(:answer)
+        allow(Answer).to receive(:create).and_return answer
+        allow(Challenge).to receive(:create).and_return nil
+        
+        post :create, name: "paul", email: "email@email.com", submitted: true, user_id: 1, 
+                      demographic_answers: [], 
+                      contact_answers: [],
+                      organization_answers: [],
+                      overview_answers: [],
+                      vision_answers: [],
+                      concern_answers: [],
+                      challenge_answers: [],
+                      referral_answers: [],
+                      geographic_answers: []
+        
+        expect(Mail::TestMailer.deliveries.length).to eq(1)
     end
   end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,4 +4,14 @@ FactoryGirl.define do
     password "password"
     password_confirmation "password"
   end
+
+  factory :question do
+    question "How old are you?"
+  end
+
+  factory :answer do
+    question_id 1
+    loi_id 1
+    answer "this is an answer"
+  end
 end
