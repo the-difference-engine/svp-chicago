2fa0e6b037b4c6008809ff15146d02a9929b8ebb
Paul <paulyk1983@gmail.com>
2016-06-27 14:16:37 -0500

    added form for creating an Attachment and added create method in attachments controller

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,4 +5,17 @@ class AttachmentsController < ApplicationController
 
   def new
   end
+
+  def create
+    @attachment = Attachment.create( attachment_params )
+  end
+
+  private
+
+  # Use strong_parameters for attribute whitelisting
+  # Be sure to update your create() and update() controller methods.
+
+  def attachment_params
+    params.require(:attachment).permit(:file)
+  end
 end
