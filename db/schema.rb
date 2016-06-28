098adee71ef19181391a60192319bfe28c519f58
Paul <paulyk1983@gmail.com>
2016-06-27 19:29:12 -0500

    added migration for adding url attribute to attachment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627202910) do
+ActiveRecord::Schema.define(version: 20160628002723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160627202910) do
     t.string   "attachment_file_content_type"
     t.integer  "attachment_file_file_size"
     t.datetime "attachment_file_updated_at"
+    t.string   "url"
   end
 
   create_table "challenges", force: :cascade do |t|
