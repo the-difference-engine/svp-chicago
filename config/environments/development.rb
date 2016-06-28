37aad872a5916128f0ac3109926f2eed1a5e6bff
Paul <paulyk1983@gmail.com>
2016-06-27 20:19:17 -0500

    tried to solve error: The bucket you are attempting to access must be addressed using the specified endpoint. Please send all future requests to this endpoint.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,9 @@ Rails.application.configure do
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-  }
+    s3_region: 'us-west-2', 
+    s3_endpoint: 's3-us-west-2.amazonaws.com'
+  },
+
 }
 end
