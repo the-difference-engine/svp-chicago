b5db2b0e6bdd92dd519a1e97116aded7e07e5084
Zeina <zeemakky@gmail.com>
2016-06-30 19:30:13 -0500

    added update/edit methods in invite_maxes controller, fixed the edit page, fixed link and added routes

--- app/controllers/invite_maxes_controller.rb
+++ app/controllers/invite_maxes_controller.rb
@@ -1,13 +1,21 @@
 class InviteMaxesController < ApplicationController
+  
+  def edit
+    @invite_max = InviteMax.find_by(id: params[:id])
+  end
+
   def update
+    @invite_max = InviteMax.find_by(id: params[:id])
+    
+    
     @invite_max.update(
       max: params[:max]
     )
-    if @rating.save 
-      redirect_to '/lois'
-      flash[:success] = "Rating Updated!"
+    if @invite_max.save 
+      render :edit
+      flash[:success] = "Number updated!"
     else
-      redirect_to :back
+      redirect_to :edit
       flash[:warning] = "Missing elements in rating!"
 
     end
