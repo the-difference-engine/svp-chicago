d6c8e9624c0d6accdc647442ff024f7899ed0ace
Paul <paulyk1983@gmail.com>
2016-07-05 14:21:31 -0500

    added name attribute to attachments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628002723) do
+ActiveRecord::Schema.define(version: 20160705192029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160628002723) do
     t.integer  "attachment_file_file_size"
     t.datetime "attachment_file_updated_at"
     t.string   "url"
+    t.string   "name"
   end
 
   create_table "challenges", force: :cascade do |t|
