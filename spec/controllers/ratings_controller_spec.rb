f1d9542f87c4669d1dd1a0fd3d0739f5d1ffbfe8
Zeina <zeemakky@gmail.com>
2016-07-05 20:16:44 -0500

    removed status from lois index page near org name

--- spec/controllers/ratings_controller_spec.rb
+++ spec/controllers/ratings_controller_spec.rb
@@ -16,4 +16,35 @@ RSpec.describe RatingsController, type: :controller do
     end
   end
 
+  describe "#create" do
+    it "should create a new rating" do
+      @rating = Rating.create(
+        q1: 1,
+        q2: 2,
+        q3: 3,
+        q4: 4,
+        q5: "yes"
+        )
+      
+      expect(@rating.q1).to eq(1)
+      expect(@rating.q5).to be_truthy
+
+    end
+  end
+
+  describe "#update" do
+    it "should create a new rating" do
+      @rating = Rating.create(
+        q1: 1,
+        q2: 2,
+        q3: 3,
+        q4: 4,
+        q5: "yes"
+        )
+      
+      expect(@rating.q1).to eq(1)
+      expect(@rating.q5).to be_truthy
+      
+    end
+  end
 end
