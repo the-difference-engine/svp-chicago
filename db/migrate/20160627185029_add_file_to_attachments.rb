f50dda14438f65f637c5ed17971be5016a6c7829
Paul <paulyk1983@gmail.com>
2016-07-09 17:58:20 -0500

    deleted add_file_to_attachments.rb migration file becuase it is no longer needed and was throwing errors on the qa server not letting me migrate

--- db/migrate/20160627185029_add_file_to_attachments.rb
+++
@@ -1,9 +0,0 @@
-class AddFileToAttachments < ActiveRecord::Migration
-  def self.up
-    add_attachment :attachments, :avatar
-  end
-
-  def self.down
-    remove_attachment :attachments, :avatar
-  end
-end
