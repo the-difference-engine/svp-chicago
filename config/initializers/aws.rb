a710c1b0cadf72f8f5072bf974f927258d2966a2
Paul <paulyk1983@gmail.com>
2016-07-14 13:51:11 -0500

    added to aws initializer file for testing on heroku qa server

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -4,4 +4,12 @@
 #   credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID'), ENV.fetch('AWS_SECRET_ACCESS_KEY'))
 # })
 
+# for version 1:
+  AWS.config({
+    region: 'us-east-1',
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] 
+  })
+
+  S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET_NAME']]
 
