5d0a3ebdd86d82378cb834e9e4a448cca7019936
Paul <paulyk1983@gmail.com>
2016-07-29 17:35:45 -0500

    added send mail command to create method in invited loi controller.

--- app/controllers/api/v1/invited_lois_controller.rb
+++ app/controllers/api/v1/invited_lois_controller.rb
@@ -15,6 +15,7 @@ class Api::V1::InvitedLoisController < ApplicationController
       loi_id: params[:loi_id]
 
     )
+    Mail.new( :to => "paulyk1983@gmail.com", :from => 'colleen@svpchicago.org', :subject => 'Invitation to Request a Proposal', :body => File.read('app/views/submission_email.html.erb')).deliver!
     render 'show.json.jbuilder'
   end
 end
