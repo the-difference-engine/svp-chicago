d34181a96c39c62c8e282dd5f5be768d2e89e544
Paul <paulyk1983@gmail.com>
2016-08-01 14:54:04 -0500

    made Mail to: dynamic instead of hard-coded. Also added content_type to display html in email.

--- app/controllers/api/v1/invited_lois_controller.rb
+++ app/controllers/api/v1/invited_lois_controller.rb
@@ -15,7 +15,13 @@ class Api::V1::InvitedLoisController < ApplicationController
       loi_id: params[:loi_id]
 
     )
-    Mail.new( :to => "paulyk1983@gmail.com", :from => 'colleen@svpchicago.org', :subject => 'Invitation to Request a Proposal', :body => File.read('app/views/submission_email.html.erb')).deliver!
+    Mail.new( 
+      :to => @invited_loi.loi.email, 
+      :from => 'colleen@svpchicago.org', 
+      :subject => 'Invitation to Request a Proposal', 
+      :body => File.read('app/views/loi_acceptance_email.html.erb')
+      :content_type => 'text/html; charset=UTF-8'
+    ).deliver!
     render 'show.json.jbuilder'
   end
 end
