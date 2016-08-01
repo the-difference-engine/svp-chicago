f2b45ce96aa90b16c01896c84d02da54b6c87ad3
Paul <paulyk1983@gmail.com>
2016-08-01 15:11:40 -0500

    added missing coma and revised email template.

--- app/controllers/api/v1/invited_lois_controller.rb
+++ app/controllers/api/v1/invited_lois_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::InvitedLoisController < ApplicationController
       :to => @invited_loi.loi.email, 
       :from => 'colleen@svpchicago.org', 
       :subject => 'Invitation to Request a Proposal', 
-      :body => File.read('app/views/loi_acceptance_email.html.erb')
+      :body => File.read('app/views/loi_acceptance_email.html.erb'),
       :content_type => 'text/html; charset=UTF-8'
     ).deliver!
     render 'show.json.jbuilder'
