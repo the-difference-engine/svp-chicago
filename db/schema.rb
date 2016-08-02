0111f654e915cee500c10a1e45e1eeec8cef41dc
Paul <paulyk1983@gmail.com>
2016-08-01 20:34:13 -0500

    added migration file to rename loi_id column to rfp_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705192029) do
+ActiveRecord::Schema.define(version: 20160802011721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,10 @@ ActiveRecord::Schema.define(version: 20160705192029) do
 
   create_table "attachments", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "loi_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "rfp_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.datetime "attachment_file_updated_at"
     t.string   "url"
     t.string   "name"
   end
