3aae59583c3cf6657a831ecbf01e780be8ffe5cd
Daniel Chu <danchu123@gmail.com>
2016-08-04 18:17:41 -0500

    created rfp_sections controller, added edit buttons on rfp_questions index

---
+++ app/controllers/rfp_sections_controller.rb
@@ -0,0 +1,31 @@
+class RfpSectionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @rfp_section = RfpSection.create(name: params[:name])
+
+    flash[:success] = "Section created!"
+    redirect_to "/rfp_questions"
+  end
+
+  def edit
+    @rfp_section = RfpSection.find(params[:id])
+  end
+  
+  def update
+    rfp_section = RfpSection.find(params[:id])
+    rfp_section.update(name: params[:section_name])
+
+    flash[:success] = "Section updated!"
+    redirect_to "/rfp_questions"
+  end
+
+  def destroy
+    rfp_section = RfpSection.find(params[:id])
+    rfp_section.destroy
+
+    flash[:success] = "Section deleted!"
+    redirect_to "/rfp_questions"
+  end
+end
