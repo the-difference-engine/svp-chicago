87191db16b3f02beaf5d494751a1208a40277559
Paul <paulyk1983@gmail.com>
2016-08-08 13:56:30 -0500

    added create logic in attachments api controller

--- app/controllers/api/v1/attachments_controller.rb
+++ app/controllers/api/v1/attachments_controller.rb
@@ -1,6 +1,26 @@
 class Api::V1::LoisController < ApplicationController
   def create
-    
+    # Make an object in your bucket for your upload
+    obj = S3_BUCKET.objects[params[:file].original_filename]
+
+    # Upload the file
+    obj.write(
+      file: params[:file],
+      acl: :public_read
+    )
+
+    # Create an object for the upload
+    @attachment = Attachment.new(
+        url: obj.public_url,
+        name: obj.key     
+        )
+
+    # Save the upload
+    if @attachment.save
+      # happy path
+    else
+      # sad path
+    end
   end
 
 end
