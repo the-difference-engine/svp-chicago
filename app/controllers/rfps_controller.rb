82a44bf9c075b21018512ef3250091ab25a0445b
AaronOoooo <aaron.o.hall@gmail.com>
2016-08-08 20:07:43 -0500

    added superadmin can view rfp

--- app/controllers/rfps_controller.rb
+++ app/controllers/rfps_controller.rb
@@ -1,8 +1,14 @@
 class RfpsController < ApplicationController
   def new
-    user_loi_status = Loi.find_by(user_id:current_user.id).status
-    
-    if !user_loi_status
+    user_has_loi = Loi.find_by(user_id:current_user.id)
+
+    if user_has_loi
+      user_loi_status = user_loi.status
+    else
+      user_loi_status = false
+    end
+
+    unless user_loi_status || current_user.super_admin
       redirect_to '/'
     end
     
