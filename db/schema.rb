0582c90529a478e69eef22d8c4c59105631f642b
Paul <paulyk1983@gmail.com>
2016-08-13 16:23:42 -0500

    added migration file to remove attachment_file_updated_at attribute and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808233406) do
+ActiveRecord::Schema.define(version: 20160813212023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,8 @@ ActiveRecord::Schema.define(version: 20160808233406) do
   create_table "attachments", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "rfp_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.datetime "attachment_file_updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "url"
     t.string   "name"
   end
