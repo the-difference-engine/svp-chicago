32151eca11f9a0d649de539a5248ae1d68c296b8
AaronOoooo <aaron.o.hall@gmail.com>
2016-09-19 20:43:40 -0500

    svp66 RFP index page

--- app/assets/javascripts/rfps_index_ctrl.js
+++ app/assets/javascripts/rfps_index_ctrl.js
@@ -4,22 +4,26 @@
   angular.module("app").controller("rfpsIndexCtrl", ["$scope", "$http", "$compile", function($scope, $http, $compile){
     window.scope = $scope;
 
-    $scope.activeId = gon.id;
+    // $scope.activeId = gon.id;
 
     $scope.setup = function(){
 
       $http.get('/api/v1/rfps.json').then(function(response){
-        $scope.rfp = response.data;
+        $scope.rfps = response.data;
 
       });
 
-      $http.get('/api/v1/rfps/' + $scope.activeId + '.json').then(function(response){
-        $scope.sections = response.data;
-        console.log($scope.sections);
+      $http.get('/api/v1/rfp_questions.json').then(function(response){
+        $scope.rfp_questions = response.data;
+
       });
+
+      // $http.get('/api/v1/rfps/' + $scope.activeId + '.json').then(function(response){
+      //   $scope.sections = response.data;
+      //   console.log($scope.sections);
+      // });
 
     };
-
   
 
   }]);
