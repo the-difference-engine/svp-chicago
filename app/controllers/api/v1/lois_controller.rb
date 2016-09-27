bc1c36decb8d32c0a4cee981b42c234c9364b2d6
AaronOoooo <aaron.o.hall@gmail.com>
2016-09-26 20:57:56 -0500

    sendgrid

--- app/controllers/api/v1/lois_controller.rb
+++ app/controllers/api/v1/lois_controller.rb
@@ -75,38 +75,44 @@ class Api::V1::LoisController < ApplicationController
         company_name = @loi.answers.find_by(question_id:1).answer
         super_admin = User.where(super_admin: true).first
 
+        UserNotifier.send_notification(current_user).deliver
+
+        User.where(super_admin: true).each do |admin|
+          UserNotifier.send_notification_admin(admin).deliver
+        end
+
         # CREATE LOI SUBMISSION CONFIRMATION EMAIL
-        File.open('app/views/submission_email.html.erb', 'w') { |file| file.write(
-        "<p>SVP Chicago has received your submission!</p>
-        <p>Please log in at <a target='_blank' href='#{request.base_url}'>#{request.base_url}</a> and review your submission</p>
-        <p>If revisions to your letter of interest form need to be made or if you have any questions, please email Colleen at colleen@svpchicago.org</p>
-        <p>Social Venture Partners - Chicago</p>"
-        ) }
+      #   File.open('app/views/submission_email.html.erb', 'w') { |file| file.write(
+      #   "<p>SVP Chicago has received your submission!</p>
+      #   <p>Please log in at <a target='_blank' href='#{request.base_url}'>#{request.base_url}</a> and review your submission</p>
+      #   <p>If revisions to your letter of interest form need to be made or if you have any questions, please email Colleen at colleen@svpchicago.org</p>
+      #   <p>Social Venture Partners - Chicago</p>"
+      #   ) }
 
-        # SEND LOI SUBMISSION CONFIRMATION EMAIL TO USER
-        Mail.new( 
-          :to => @loi.email, 
-          :from => 'colleen@svpchicago.org', 
-          :subject => 'Submission to SVP received', 
-          :body => File.read('app/views/submission_email.html.erb'),
-          :content_type => 'text/html; charset=UTF-8'
-        ).deliver!
+      #   # SEND LOI SUBMISSION CONFIRMATION EMAIL TO USER
+      #   Mail.new( 
+      #     :to => @loi.email, 
+      #     :from => 'colleen@svpchicago.org', 
+      #     :subject => 'Submission to SVP received', 
+      #     :body => File.read('app/views/submission_email.html.erb'),
+      #     :content_type => 'text/html; charset=UTF-8'
+      #   ).deliver!
 
-        # CREATE ADMIN ALERT EMAIL TEMPLATE
-        File.open('app/views/loi_submitted_email_to_admin.html.erb', 'w') { |file| file.write(
-        "<p>A user from <i>#{@loi.org_name}</i> has submitted a letter of interest.</p>
-        <p>Click below to log in and review their LOI:</p>
-        <p><a target='_blank' href='#{request.base_url}'>#{request.base_url}</a></p>"
-        ) }
+      #   # CREATE ADMIN ALERT EMAIL TEMPLATE
+      #   File.open('app/views/loi_submitted_email_to_admin.html.erb', 'w') { |file| file.write(
+      #   "<p>A user from <i>#{@loi.org_name}</i> has submitted a letter of interest.</p>
+      #   <p>Click below to log in and review their LOI:</p>
+      #   <p><a target='_blank' href='#{request.base_url}'>#{request.base_url}</a></p>"
+      #   ) }
 
-        # SEND LOI SUBMISSION ALERT EMAIL TO ADMIN
-        Mail.new( 
-          :to => super_admin.email,
-          :from => 'svptesting1871@gmail.com', 
-          :subject => 'A letter of interest has been submitted', 
-          :body => File.read('app/views/loi_submitted_email_to_admin.html.erb'),
-          :content_type => 'text/html; charset=UTF-8'
-        ).deliver!        
+      #   # SEND LOI SUBMISSION ALERT EMAIL TO ADMIN
+      #   Mail.new( 
+      #     :to => super_admin.email,
+      #     :from => 'svptesting1871@gmail.com', 
+      #     :subject => 'A letter of interest has been submitted', 
+      #     :body => File.read('app/views/loi_submitted_email_to_admin.html.erb'),
+      #     :content_type => 'text/html; charset=UTF-8'
+      #   ).deliver!        
       end
 
       if errors.empty?
