3dea6222002f4d89934911b3dc53fbf7b9f4e6c1
Daniel Chu <danchu123@gmail.com>
2016-09-26 21:09:50 -0500

    Rfp index in progress

--- app/assets/javascripts/rfps_index_ctrl.js
+++ app/assets/javascripts/rfps_index_ctrl.js
@@ -13,7 +13,7 @@
 
       $http.get('/api/v1/rfps.json').then(function(response){
         $scope.rfps = response.data;
-
+        console.log($scope.rfps);
       });
 
       $http.get('/api/v1/rfp_questions.json').then(function(response){
@@ -38,8 +38,35 @@
       else if(!$scope.descending){
         $scope.toggle_class = "glyphicon glyphicon-triangle-top"
       }
-    }
-  
+    };
+
+    $scope.formatAnswer = function(answerToFormat){
+      var subAnswerFormat = [];
+
+      if (answerToFormat && answerToFormat.rfp_answer === "See Subs"){
+        for(var i = 0; i < answerToFormat.sub_answers.length; i++){
+          subAnswerFormat.push(answerToFormat.sub_answers[i].sub_question + ":  " + answerToFormat.sub_answers[i].sub_answer);
+        }
+
+        return subAnswerFormat;
+      }
+      else if(answerToFormat){
+        subAnswerFormat.push(answerToFormat.rfp_answer);
+
+        return subAnswerFormat;
+      }
+      // if (answerToFormat.sub_questions.length !== 0){
+
+      // } else {
+      //   answerToFormat["rfp_answer"]
+      // }
+      // if(answerToFormat === "See Subs"){
+      //   return answerToFormat["sub_answers"];
+      // }
+      // else{
+      //   return answerToFormat;
+      // }
+    };
 
   }]);
 }());
