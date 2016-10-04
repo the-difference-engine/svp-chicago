d7bcf83d4c39e98f2cf8f71b59180810c1fab107
heydanhey <danhey@gmail.com>
2016-10-04 07:52:16 -0500

    remove mail config

--- config/initializers/mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
- :user_name => ENV['SENDGRID_USER'],
- :password => ENV['SENDGRID_PASSWORD'],
- :domain => 'prod-svp-chicago.herokuapp.com',
- :address => "smtp.sendgrid.net",
- :port => 587,
- :authentication => :plain,
- :enable_starttls_auto => true
-}
