cab9129dd921010f3ac0b59c6f6a9ad4f9f5ed77
Profession-Programing <jjfisher91@gmail.com>
2016-11-10 19:58:45 -0600

    rfp_rating_weighted_score_method_added

--- app/assets/javascripts/rfp_ratings_ctrl.js
+++ app/assets/javascripts/rfp_ratings_ctrl.js
@@ -5,10 +5,15 @@
   angular.module("app").controller("RfpRatingsCtrl", ["$scope", "$http", "$compile", function($scope, $http, $compile){
     window.scope = $scope;
 
+    //GET CURRENT USER ID USING GON GEM
+    $scope.currentUserId = gon.current_user_id;
+
     $scope.setup = function(){
 
       $http.get('/api/v1/rfp_ratings.json').then(function(response){
-        $scope.rfp_ratings = response;
+        $scope.rfp_ratings = response.data;
+        console.log(response);
+        console.log($scope.rfp_ratings);
       });
 
     }
