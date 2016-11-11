dd8a381e52e02fb6c294f56d491cba6d5e1201be
Profession-Programing <jjfisher91@gmail.com>
2016-11-10 20:47:05 -0600

    end_of_day

--- app/assets/javascripts/rfp_ratings_ctrl.js
+++ app/assets/javascripts/rfp_ratings_ctrl.js
@@ -9,7 +9,6 @@
     $scope.currentUserId = gon.current_user_id;
 
     $scope.setup = function(){
-
       $http.get('/api/v1/rfp_ratings.json').then(function(response){
         $scope.rfp_ratings = response.data;
         console.log(response);
