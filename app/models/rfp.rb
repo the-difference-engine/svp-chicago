4eca7fd326d55e69466d1091b1c5f3a0211303ad
rwscherrer <rwscherrer@gmail.com>
2016-12-01 18:23:55 -0600

    removed_commented_out_code

--- app/models/rfp.rb
+++ app/models/rfp.rb
@@ -13,40 +13,7 @@ class Rfp < ActiveRecord::Base
     end
   end
 
-  # def self.to_csv
-  #   attributes = %w{id name email}
-    
-  #   questions = Question.order(:section_id).order(:id).all
-  #   attribute_questions = []
-  #   questions.each do|question|
-  #     attribute_questions << question.question
-  #   end
-
-  #   CSV.generate(headers: true) do |csv|
-  #     csv << attributes + attribute_questions
-  #     all.each do |loi|
-  #       attribute_answers = []
-  #       loi.answers.each do |answer|
-  #         if answer.challenges.empty?
-  #           attribute_answers << answer.answer
-  #         else
-  #           challenges = ""
-  #           answer.challenges.each do |challenge|
-  #             challenges += "#{challenge.challenge}: #{challenge.priority}. "
-  #           end
-
-  #           if answer.fte
-  #             challenges += concat_ftes(answer.fte)
-  #           end
-
-  #           attribute_answers << challenges
-  #         end
-  #       end
-  #       csv << loi.attributes.values_at(*attributes) + attribute_answers
-  #     end
-  #   end
-  # end
-
+  
 
   def friendly_loi_time
     updated_at.strftime("%m/%d/%Y")
