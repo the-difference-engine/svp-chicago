0906df8470dfc91400d362b9f2e2edb79a1364da
rwscherrer <rwscherrer@gmail.com>
2016-12-08 20:50:20 -0600

    added partial logic for csv generation

--- app/models/rfp.rb
+++ app/models/rfp.rb
@@ -13,6 +13,24 @@ class Rfp < ActiveRecord::Base
     end
   end
 
+  def self.to_csv
+    attributes = %w{id name email}
+    attribute_questions = []
+    questions.each do |question|
+      attribute_questions << question.question
+    end
+
+    CSV.generate(headers: true) do |csv|
+      csv << attritbutes + attribute_questions
+      all.each do |rfp|
+        attribute_answers = []
+        rfp.answers.each do |answer|
+          attribute_answers << answer.answer
+        end
+      end
+    end
+  end
+
   
 
   def friendly_loi_time
