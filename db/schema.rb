eff744e829beb5649c417cf0dabdd38feb3f4d7e
Profession-Programing <jjfisher91@gmail.com>
2016-12-22 20:32:31 -0600

    create Dashboard without typo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161223014310) do
+ActiveRecord::Schema.define(version: 20161223023136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20161223014310) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "dashboads", force: :cascade do |t|
+  create_table "dashboards", force: :cascade do |t|
     t.boolean  "open?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
