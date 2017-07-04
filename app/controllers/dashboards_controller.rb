7271f9ed3a5a5d4a8c297ae8181b562199169b5f
Daniel Hey <danhey@daniel-hey.attlocal.net>
2017-07-03 20:55:12 -0500

    added DateTime parsing

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,7 @@ class DashboardsController < ApplicationController
     # Fat Models slim controllers
     @dashboard = Dashboard.find(params[:id])
     date = params[:loi_closing_date] + ' 23:59:59'
-    @dashboard.update(loi_closing_date: date)
+    @dashboard.update(loi_closing_date: DateTime.parse(date))
     if @dashboard.save
         redirect_to "/"
         flash[:success] = "Deadline Updated"
